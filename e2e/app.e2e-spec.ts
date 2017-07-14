11689dbc1a383c9d15cd7a1e64ac158a790e7b89
Angular CLI <angular-cli@angular.io>
2017-07-14 12:47:45 -0700

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { SuggestionBoxPage } from './app.po';
+
+describe('suggestion-box App', () => {
+  let page: SuggestionBoxPage;
+
+  beforeEach(() => {
+    page = new SuggestionBoxPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
